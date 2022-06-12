3943ae1547003fd06e6d6a0e3c57dc6a24c973ab
MayurBhowad <mayurbhowad1995@gmail.com>
2022-06-13 01:03:09 +0530

    added: light and dark theme

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import { signOut } from 'next-auth/react'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-900'>
       <Head>
         <title>LinkedIN</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <button onClick={signOut} >Sign out</button>
+      <Header />
+
+      <main>
+        <div>
+          {/* Sidebar */}
+          {/* Feed */}
+        </div>
+        {/* Widgets */}
+      </main>
     </div>
   )
 }
