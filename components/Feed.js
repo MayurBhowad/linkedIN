2002c66dca07966f03784cccf6f84ebb7507f61b
MayurBhowad <mayurbhowad1995@gmail.com>
2022-06-20 00:11:35 +0530

    added: add feed post input design

---
+++ components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Input from './Input'
+
+function Feed() {
+    return (
+        <div className='space-y-6 pb-24 max-w-lg'>
+            <Input />
+            {/* Post */}
+        </div>
+    )
+}
+
+export default Feed
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,54 @@
-import { signOut } from 'next-auth/react'
+import { getSession, signOut, useSession } from 'next-auth/react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
+  const router = useRouter()
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/home')
+    }
+  })
+
   return (
-    <div className='bg-gray-900'>
+    <div className='bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6'>
       <Head>
-        <title>LinkedIN</title>
+        <title>Feed | LinkedIN</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
 
-      <main>
-        <div>
-          {/* Sidebar */}
-          {/* Feed */}
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className='flex flex-col md:flex-row gap-5'>
+          <Sidebar />
+          <Feed />
         </div>
         {/* Widgets */}
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  //Check the user is authenticated on the server...
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/home',
+      }
+    }
+  }
+
+  return {
+    props: {
+      session,
+    }
+  }
 }
